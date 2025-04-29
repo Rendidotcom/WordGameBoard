16602be4b6d9c7b7e7d41ab3f018f8747cd55aac
Rendi Nur Holis <holis112@gmail.com>
2025-04-29 09:36:26 +0700

    Add supabase client

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,9 +1,6 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = { supabase };
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);

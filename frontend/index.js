55311f690ed0d290082bf5f247789280c67e4804
Rendi Nur Holis <holis112@gmail.com>
2025-04-30 10:16:47 +0700

    Add audio file and implement sound playback

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.body.innerHTML += "<p>JS berhasil dimuat!</p>";
   });
-  
+  const klikSound = new Audio("audio/klik.mp3");
+klikSound.play();
